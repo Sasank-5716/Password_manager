965b846f5cb66da28afc62567ba7821bba17bec6
Sasank5716 <sasanklama5716@gmail.com>
2025-03-30 23:25:15 +0545

    second commit

--- Password_manager.py
+++ Password_manager.py
@@ -8,16 +8,33 @@ import getpass
 import argparse
 
 class PasswordManager:
+    
+    def _get_fernet(self):
+        # Load or generate encryption key
+        try:
+            with open(self.key_file, "rb") as f:
+                key = f.read()
+        except FileNotFoundError:
+            key = Fernet.generate_key()
+            with open(self.key_file, "wb") as f:
+                f.write(key)
+    
+        return Fernet(key)
+
+    def _hash_password(self, password):
+        return hashlib.sha256(password.encode()).hexdigest()
+
+    
     def __init__(self, db_file="passwords.db"):
         self.db_file = db_file
         self.key_file = "secret.key"
-        self.fernet = None
+        self.fernet = self._get_fernet()
     
-    # Other methods will be added in subsequent commits
+    
 
 def main():
     parser = argparse.ArgumentParser(description="Password Manager")
-    # Argument parsing will be added in subsequent commits
+    
     pass
 
 if __name__ == "__main__":
